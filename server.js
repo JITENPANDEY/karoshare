6451dd571f38c2cff9a191622937342701642987
Jiten Pandey <devendrap@flock.com>
2020-09-08 00:04:07 +0530

    add cors

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const path = require('path');
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+    origin:'http://localhost:3000'
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
 
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+
+app.use(cors(corsOptions))
 app.use(express.static('public'));
 
 const connectDB = require('./config/db')
@@ -14,6 +29,10 @@ app.set('view engine','ejs');
 
 app.use(express.json());
 
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
 //Routes
 app.use('/api/files',require('./routes/files.js'))
 app.use('/files',require('./routes/show.js'))
