037a529d69fb555323e43e072b25991edbcb1ca7
Jiten Pandey <devendrap@flock.com>
2020-09-09 19:17:53 +0530

    email setup

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -153,7 +153,7 @@ module.exports = ({ emailFrom, downloadLink, size, expires }) => {
                             </tr>
                             <tr>
                             <td class="content-block powered-by" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                Powered by <a href="https://jitenpandey.herokuapp.com/" style="color: #999999; font-size: 12px; text-align: center; text-decoration: none;">Jiten Pandey</a>.
+                                Powered by <p style="color: #999999; font-size: 12px; text-align: center; text-decoration: none;">Jiten Pandey</p>.
                             </td>
                             </tr>
                         </table>
